bd104d458ec87c13baf1333d2a7836105f1226e5
Jon Jagger <jon@jaggersoft.com>
2020-09-03 09:19:54 +0100

    Use bash env-var for sandbox dir

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,21 +1,20 @@
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
-# It seems jest cannot use ts-jest when installed globally.
-# Because of this, all npm packages are installed in /etc/ts
-ln -s /etc/ts/node_modules /sandbox/node_modules
+# jest cannot use ts-jest when installed globally.
+# Image installs all npm packages in /etc/ts/
+ln -s /etc/ts/node_modules ${CYBER_DOJO_SANDBOX}/node_modules
 
 #if [ -f .jshintrc ]; then
-#  jshint --config .jshintrc *.js
+#  jshint --config .jshintrc *.ts
 #fi
 
 if [ $? == 0 ]; then
-  # jest does not appear to have a cli option to turn off ansi
-  # escape codes. So I am saving stderr to a file, then
-  # stripping the ansi codes from the file, and catting the
-  # result back to stderr.
+  # jest has no cli option to turn off ansi escape codes.
+  # So save stderr to a file, then strip the ansi codes from the
+  # file, then cat the result back to stderr.
   STDERR_LOG_FILE=/tmp/ts-jest.ascii-coloured.log
   npm run test 2> "${STDERR_LOG_FILE}"
   STATUS=$?
-  /sandbox/node_modules/.bin/strip-ansi < "${STDERR_LOG_FILE}" >&2
+  ${CYBER_DOJO_SANDBOX}/node_modules/.bin/strip-ansi < "${STDERR_LOG_FILE}" >&2
   exit ${STATUS}
 fi
