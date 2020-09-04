d1a1090a6884eb02f5f9328151d84a50a1cb70c2
Jon Jagger <jon@jaggersoft.com>
2020-09-04 16:51:57 +0100

    Add trap handler to cyber-dojo.sh

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,6 +1,14 @@
 # jest cannot use ts-jest when installed globally.
-# Image installs all npm packages in /etc/ts/
+# npm packages are in /etc/ts/
+# We have to create a local symlink.
 ln -s /etc/ts/node_modules ${CYBER_DOJO_SANDBOX}/node_modules
+
+function cyber_dojo_exit()
+{
+  # Ensure the symlink is removed.
+  unlink ${CYBER_DOJO_SANDBOX}/node_modules
+}
+trap cyber_dojo_exit EXIT SIGTERM
 
 npm run typecheck || exit 42
 
