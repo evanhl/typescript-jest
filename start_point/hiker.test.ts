f3c486247112d5014d9816ede0752054c50a2391
Evan Owski <evan.owski@folsomlabs.com>
2022-08-31 13:58:13 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:start_point/hiker.test.ts

--- start_point/hiker.test.ts
+++ start_point/hiker.test.ts
@@ -8,25 +8,35 @@ describe('matching_braces', () => {
     it('non-braces', () => {
       expect(areBracesMatching('awefoi')).toEqual(true);
     });
+
     // two sets of same braces
+
     it('single set of braces w/ nothing inside', () => {
       expect(areBracesMatching('()')).toEqual(true);
       expect(areBracesMatching('[]')).toEqual(true);
       expect(areBracesMatching('{}')).toEqual(true);
     });
-  )});
+  });
   describe('standard cases', () => {
-    // single set of braces w/ something inside
     it('single set of braces w/ something inside', () => {
-      expect(areBracesMatching('()')).toEqual(true);
-      expect(areBracesMatching('[]')).toEqual(true);
-      expect(areBracesMatching('{}')).toEqual(true);
+      expect(areBracesMatching('(ab)')).toEqual(true);
+      expect(areBracesMatching('[ab]')).toEqual(true);
+      expect(areBracesMatching('{ab}')).toEqual(true);
     });
+
     // two sets of different braces properly nested
     // two sets of different braces improperly nested
-    // unclosed single brace
+
+    it('unclosed single brace is invalid', () => {
+      expect(areBracesMatching('(')).toEqual(false);
+      expect(areBracesMatching('[')).toEqual(false);
+      expect(areBracesMatching('{')).toEqual(false);
+    });
+
+
     // unclosed multiple braces ([(
     // open-closed-open-closed
     // open-closed-open ()(
+    // too many closing braces
   });
 });
--- start_point/hiker.ts
+++ start_point/hiker.ts
@@ -1,3 +1,12 @@
 export function areBracesMatching(bracesStr: string): boolean {
-  return false;
+  const stack = [];
+  // use stack
+  // ignore non-brace chars
+
+
+  // when reading closing brace, check to see if stack.length === 0
+
+  // at the end return false if stack is > 0 elements
+
+  return stack.length === 0;
 }
