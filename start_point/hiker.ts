a56fd8403c5605b1d46a21635669dc520aa0788a
Evan Owski <evan.owski@folsomlabs.com>
2022-08-31 14:16:10 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:start_point/hiker.ts

--- start_point/hiker.ts
+++ start_point/hiker.ts
@@ -1,9 +1,37 @@
+
+const BRACES = ['{', '}', '[', ']', '(', ')'];
+
+const OPEN_TO_CLOSE_MAPPING: {[key: string]: string} = {
+  '{': '}',
+  '[': ']',
+  '(': ')',
+};
+
 export function areBracesMatching(bracesStr: string): boolean {
   const stack = [];
-  // use stack
   // ignore non-brace chars
-  for (const ch of bracesStr) {
+  for (let i = 0; i < bracesStr.length; i++) {
+    const ch = bracesStr.charAt(i);
 
+    if (BRACES.includes(ch)) {
+      if (OPEN_TO_CLOSE_MAPPING[ch]) {
+        stack.push(ch);
+        // if closed brace
+      } else if () {
+        // check top of stack
+        const lastOpeningBrace = stack[stack.length - 1];
+
+        if (!lastOpeningBrace) { return false; }
+        // if top of stack is matching, then pop
+        if ()
+        // else return false
+      }
+
+
+    } else {
+      // TODO: maybe delete
+      continue;
+    }
   }
 
   // when reading closing brace, check to see if stack.length === 0
